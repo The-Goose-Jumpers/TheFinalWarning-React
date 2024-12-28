33c34e9fe2febcd024c21b92b3c2ca5167c02d7e
StormGhozt <vitordanieln@gmail.com>
2024-12-28 16:42:32 +0000

    adding missing file

---
+++ src/data/ImageLoad.js
@@ -0,0 +1,15 @@
+const Scenes = {
+    LivingRoom: "/images/Scenes/LivingRoom.png",
+    Driving: "/images/Scenes/driving.png",
+    FrontDoor: "/images/Scenes/frontdoor.jpg",
+    Outside: "/images/Scenes/outside house.png",
+    Hotel: "/images/Scenes/hotelsign.jpg",
+};
+
+const UI = {
+    textBox: "/images/UI/textbox.svg",
+    clockBox: "/images/UI/clockbox.svg",
+    pause: "/images/UI/pause.svg",
+}
+
+export {Scenes, UI};
